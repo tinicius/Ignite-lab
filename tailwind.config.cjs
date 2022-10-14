981fa05419f9154fee31ab11e03dab7d814e2e16
Vinicius Alves Pereira <vinicius.apereira04@gmail.com>
2022-10-14 15:08:04 -0300

    creating components

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -15,14 +15,25 @@ module.exports = {
     },
 
     colors: {
-      'gray-900': '#121214',
-      'gray-800': '#202024',
-      'gray-400': '#7c7c8a',
-      'gray-200': '#c4c4cc',
-      'gray-100': '#e1e1e6',
 
-      'cyan-500': '#81d8f7', 
-      'cyan-300': '#a7e8ff', 
+      transparent: 'transparent',
+
+      'black': '#000',
+      'white': '#FFF',
+
+      gray: {
+        900: '#121214',
+        800: '#202024',
+        400: '#7c7c8a',
+        200: '#c4c4cc',
+        100: '#e1e1e6',
+      },
+
+      cyan: {
+        500: '#81d8f7', 
+        300: '#a7e8ff', 
+      },
+
     },
     extend: {
       fontFamily: {
